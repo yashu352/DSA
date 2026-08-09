f39de59538cc469942850801444add73c7373fef
Jay Chede <yashuchede@gmail.com>
2026-08-10 01:28:48 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (13.57%) - LeetSync

--- 344-reverse-string/reverse-string.c
+++ 344-reverse-string/reverse-string.c
@@ -2,9 +2,9 @@ void reverseString(char* s, int sSize)
 {
     int left=0;
     int right=sSize-1;
+    char temp;
     while(left<right)
     {
-        char temp;
         temp=s[left];
         s[left]=s[right];
         s[right]=temp;
